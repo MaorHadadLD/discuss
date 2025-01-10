d9b87c138de8a3f81e92f188135cf328570204e4
MaorHadadLD <maorhadad94@gmail.com>
2025-01-10 18:34:20 +0200

    Showing Loading Spinners

--- discuss/src/components/topics/topic-create-form.tsx
+++ discuss/src/components/topics/topic-create-form.tsx
@@ -10,6 +10,7 @@ import {
     PopoverContent
 } from '@nextui-org/react';
 import * as actions from '@/actions';
+import FormButton from '@/components/common/form-button';
 
 export default function TopicCreateForm() {
     const [formState, action] = useActionState(actions.createTopic, {
@@ -25,27 +26,28 @@ export default function TopicCreateForm() {
                 <form action={action}>
                     <div className="flex flex-col gap-4 p-4 w-80">
                         <h3 className="text-lg">Create a Topic</h3>
-                    </div>
-                    <Input 
+                    
+                        <Input 
                         name="name" 
                         label="Name" 
                         labelPlacement="outside" 
                         placeholder="Name"
                         isInvalid={!!formState.errors.name}
                         errorMessage={formState.errors.name?.join(', ')}
-                     /> 
-                    <Textarea
+                        /> 
+                        <Textarea
                         name="description"
                         label="Description"
                         labelPlacement="outside"
                         placeholder="Describe your topic"
                         isInvalid={!!formState.errors.description}
                         errorMessage={formState.errors.description?.join(', ')}
-                    />
+                        />
 
-                    {formState.errors._form ? <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form?.join(', ')}</div> : null}
+                        {formState.errors._form ? <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form?.join(', ')}</div> : null}
 
-                    <Button type="submit" color="primary">Create</Button>
+                        <FormButton>Create Topic</FormButton>
+                    </div>
                 </form>
             </PopoverContent>
         </Popover>
