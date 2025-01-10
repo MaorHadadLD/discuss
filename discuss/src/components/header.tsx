7d1190cf50720c9d4a5578ed958bd95328ea0da9
MaorHadadLD <maorhadad94@gmail.com>
2025-01-10 10:20:48 +0200

    Enabling SignIn

--- discuss/src/components/header.tsx
+++ discuss/src/components/header.tsx
@@ -9,9 +9,33 @@ import {
     Avatar
 } from '@nextui-org/react';
 import { auth } from '@/auth';
+import * as actions from '@/actions';
 
 export default async function Header() {
     const session = await auth();
+
+    let authContent: React.ReactNode;
+    if(session?.user) {
+        authContent = <Avatar src={session.user.image || ''}/>
+    } else {
+        authContent = <>
+            <NavbarItem>
+                <form action={actions.signIn}>
+                    <Button type="submit" color="secondary" variant="bordered">
+                        Sign In
+                    </Button>
+                </form>
+            </NavbarItem>
+
+            <NavbarItem>
+                <form action={actions.signIn}>
+                    <Button type="submit" color="primary" variant="flat">
+                        Sign Up
+                     </Button>
+                </form>
+            </NavbarItem>
+        </>
+    }
 
     return (
         <Navbar className="shadow mb-6">
@@ -24,14 +48,7 @@ export default async function Header() {
                 </NavbarItem>
             </NavbarContent>
                 
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    {
-                        session?.user ? <div>Sined In</div> : <div>Signed Out</div>
-                    }
-                </NavbarItem>
-
-            </NavbarContent>
+            <NavbarContent justify="end"> { authContent } </NavbarContent>
             
         </Navbar>
     )
