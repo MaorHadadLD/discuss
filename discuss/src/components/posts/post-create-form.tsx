b6e464975de497a2d04260f931d1ec166837e082
MaorHadadLD <maorhadad94@gmail.com>
2025-01-10 20:00:55 +0200

    Checking Authentication Status

--- discuss/src/components/posts/post-create-form.tsx
+++ discuss/src/components/posts/post-create-form.tsx
@@ -48,6 +48,13 @@ export default function PostCreateForm() {
                             labelPlacement="outside"
                             placeholder="Content"
                         />
+                        
+                        {
+                            formState.errors._form ? (
+                            <div className="rounded p-2 bg-red-200 border border-res-400">{formState.errors._form.join(', ')}</div>
+                         ) : null
+                        }
+
 
                         <FormButton>
                             Create Post
